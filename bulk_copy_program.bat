df1c297a6ba71d3e09b61c1f800910ba9e7e116d
datajoedata <116616136+datajoedata@users.noreply.github.com>
2023-07-28 22:15:32 -0300

    Update bulk_copy_program.bat

--- bulk_copy_program.bat
+++ bulk_copy_program.bat
@@ -1,13 +1,28 @@
+REM The lines starting with REM are comments that explain what each variable is for.
+
 @echo off
 
-set "server=DESKTOP-AV0JTTN"
-set "database=PROJECT2_DRUG_RESISTANT_ANTIBIOTICS"
+REM 1-  Setting up variables like server, database, schema, table, and folder path: 
+
+set "server= your_server_here"
+
+set "database= your_database_here"
+
 set "schema=dbo"
-set "table=industrialized_meds01"
+
+set "table=your_target_table_name_here"
+
 set "folder_path=C:\Users\ninol\Desktop\PROFESSIONAL\DATA_STORAGE\MEDICAMENTOS"
 
+
+
+REM 2- This code below Loops through all CSV files in the specified folder
+
 for %%F in ("%folder_path%\*.csv") do (
+    REM Using BCP (Bulk Copy Program) to import data from each CSV file to SQL Server 
     bcp "%database%.%schema%.%table%" in "%%~fF" -S %server% -T -c -t";" -r "\n" -C 1252
 )
 
-echo Importação de todos os arquivos concluída.2
+REM Displaying a message indicating the completion of the import process for all files
+
+echo Data import completed.
