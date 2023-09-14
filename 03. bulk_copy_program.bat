7b9bb8a3c3b74c3e679053be2c42d83ac8ff40be
datajoedata <116616136+datajoedata@users.noreply.github.com>
2023-09-14 20:09:47 -0300

    Update 03. bulk_copy_program.bat

--- 03. bulk_copy_program.bat
+++ 03. bulk_copy_program.bat
@@ -7,24 +7,25 @@ REM The bulk copy program (bcp) utility bulk copies data between an instance of 
 
 REM 1-  Setting up variables like server, database, schema, table, and folder path: ###########
 
-set "server= your_server_here"
+  
+  set "server= your_server_here"
 
-set "database= your_database_here"
+  set "database= your_database_here"
 
-set "schema=dbo"
+  set "schema=dbo"
 
-set "table=your_target_table_name_here"
+  set "table=your_target_table_name_here"
 
-set "folder_path=your_folder_path_here"
+  set "folder_path=your_folder_path_here"
 
 
 
 REM 2- This code below Loops through all CSV files in the specified folder  ###########
 
-for %%F in ("%folder_path%\*.csv") do (
-    REM Using BCP (Bulk Copy Program) to import data from each CSV file to SQL Server 
-    bcp "%database%.%schema%.%table%" in "%%~fF" -S %server% -T -c -t";" -r "\n" -C 1252
-)
+  for %%F in ("%folder_path%\*.csv") do (
+      REM Using BCP (Bulk Copy Program) to import data from each CSV file to SQL Server 
+      bcp "%database%.%schema%.%table%" in "%%~fF" -S %server% -T -c -t";" -r "\n" -C 1252
+  )
 
 REM Displaying a message indicating the completion of the import process for all files  ###########
 
