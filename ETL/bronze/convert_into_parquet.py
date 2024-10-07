9b9cdf30339d74472969e331dcdb1a6f26117e6e
datajoedata <116616136+datajoedata@users.noreply.github.com>
2024-10-07 18:54:25 -0300

    Update convert_into_parquet.py

--- ETL/bronze/convert_into_parquet.py
+++ ETL/bronze/convert_into_parquet.py
@@ -1,25 +1,17 @@
-import os
+import os 
 import json
 import pandas as pd
-from utilities import load_config_and_set_env
+from utilities import set_environment_variables, get_file_paths, parquet_exists, get_output_folder_for_file_type 
 
-# Carregar o arquivo config.json e definir variáveis de ambiente
-config = load_config_and_set_env('\\bacteria_project\\config.json')
+# Carregar o arquivo config.json
+with open('C:\\Users\\ninol\\Desktop\\PROFESSIONAL\\projects\\bacteria_project\\config.json', 'r') as file:
+    config = json.load(file)
 
 # Chamar a função set_environment_variables
 set_environment_variables(config)
 
-# Função para listar os arquivos dentro de um diretório
-def get_file_paths(folder_path):
-    return [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-
-# Função para verificar se o arquivo Parquet já existe
-def parquet_exists(file_path, output_folder):
-    parquet_path = os.path.join(output_folder, os.path.basename(file_path).replace('.csv', '.parquet').replace('.xls', '.parquet').replace('.xlsx', '.parquet'))
-    return os.path.exists(parquet_path)
-
 # Função para converter arquivos CSV, XLS, ou XLSX para o formato Parquet
-def convert_to_parquet(file_path, output_folder):
+def convert_to_parquet(file_path, output_folder, encoding, selected_columns=None):
     try:
         if parquet_exists(file_path, output_folder):
             print(f"Arquivo {file_path} já convertido para Parquet. Pulando...")
@@ -28,7 +20,7 @@ def convert_to_parquet(file_path, output_folder):
         # Converter CSV para Parquet
         if file_path.endswith(".csv"):
             print(f"Convertendo arquivo CSV {file_path} para Parquet...")
-            df = pd.read_csv(file_path, delimiter=';', encoding='latin-1', dtype=str)
+            df = pd.read_csv(file_path, delimiter=';', encoding=encoding, dtype=str, usecols=selected_columns)
             parquet_path = os.path.join(output_folder, os.path.basename(file_path).replace('.csv', '.parquet'))
             df.to_parquet(parquet_path, index=False)
             print(f"Arquivo {file_path} convertido para {parquet_path}.")
@@ -36,27 +28,18 @@ def convert_to_parquet(file_path, output_folder):
         # Converter Excel (XLS/XLSX) para Parquet
         elif file_path.endswith(".xls") or file_path.endswith(".xlsx"):
             print(f"Convertendo arquivo Excel {file_path} para Parquet...")
-            df = pd.read_excel(file_path, engine='openpyxl', dtype=str)
+            df = pd.read_excel(file_path, engine='openpyxl', dtype=str, usecols=selected_columns)
             parquet_path = os.path.join(output_folder, os.path.basename(file_path).replace('.xls', '.parquet').replace('.xlsx', '.parquet'))
             df.to_parquet(parquet_path, index=False)
             print(f"Arquivo {file_path} convertido para {parquet_path}.")
-        
+
         else:
             print(f"Formato de arquivo não suportado: {file_path}")
 
+    except UnicodeDecodeError as e:
+        print(f"Erro ao converter {file_path} para Parquet: Problema de codificação - {str(e)}")
     except Exception as e:
         print(f"Erro ao converter {file_path} para Parquet: {str(e)}")
-
-# Função para definir as pastas de saída conforme o tipo de arquivo
-def get_output_folder_for_file_type(folder_name):
-    if 'industrialized' in folder_name:
-        return os.environ['INDUSTRIALIZED_PARQUET']
-    elif 'manipulated' in folder_name:
-        return os.environ['MANIPULATED_PARQUET']
-    elif 'raw_sim_files' in folder_name:
-        return os.environ['SIM_PARQUET']
-    else:
-        return os.environ['IBGE_PARQUET']
 
 # Processar os arquivos e convertê-los em Parquet
 for folder_name, folder_path in config['folder_paths'].items():
@@ -67,7 +50,23 @@ for folder_name, folder_path in config['folder_paths'].items():
     output_folder = get_output_folder_for_file_type(folder_name)
     os.makedirs(output_folder, exist_ok=True)
 
+    # Obter a codificação específica para a pasta
+    encoding = config['folder_encodings'].get(folder_name, 'cp1252')  # Usando 'cp1252' como padrão
+
+    # Selecionar colunas apenas para 'raw_sim_files'
+    selected_columns = None
+    if folder_name == 'raw_sim_files':
+        selected_columns = ['CONTADOR', 'DTOBITO', 'NATURAL', 'CODMUNNATU', 'IDADE', 'SEXO', 'CODMUNRES', 'CODESTAB',
+                            'CODMUNOCOR', 'LINHAA', 'LINHAB', 'LINHAC', 'LINHAD', 'LINHAII', 'CAUSABAS', 'CAUSABAS_O',
+                            'DTCADASTRO', 'ATESTADO', 'CAUSAMAT', 'STDOEPIDEM']
+
     # Listar arquivos dentro da pasta e convertê-los para Parquet
     file_paths = get_file_paths(absolute_path)
+    processed_files_count = 0
     for file_path in file_paths:
-        convert_to_parquet(file_path, output_folder)
+        convert_to_parquet(file_path, output_folder, encoding, selected_columns if folder_name == 'raw_sim_files' else None)
+        processed_files_count += 1
+
+    print(f"Processamento da pasta '{folder_name}' concluído. Total de arquivos convertidos: {processed_files_count}")
+
+print("Processamento finalizado.")
