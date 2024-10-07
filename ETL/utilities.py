d29b9b1b69c32a5fd0379691257fcfb944f18f90
datajoedata <116616136+datajoedata@users.noreply.github.com>
2024-10-07 18:55:11 -0300

    Update utilities.py

--- ETL/utilities.py
+++ ETL/utilities.py
@@ -36,3 +36,30 @@ def set_environment_variables(config):
     for key, value in config.get('output_folders', {}).items():
         os.environ[key.upper()] = value
         print(f"Variável de ambiente {key.upper()} definida com sucesso.")
+
+
+
+
+
+# Função para listar os arquivos dentro de um diretório
+def get_file_paths(folder_path):
+    return [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+
+
+# Função para verificar se o arquivo Parquet já existe
+def parquet_exists(file_path, output_folder):
+    parquet_path = os.path.join(output_folder, os.path.basename(file_path).replace('.csv', '.parquet').replace('.xls', '.parquet').replace('.xlsx', '.parquet'))
+    return os.path.exists(parquet_path)
+
+
+# Função para definir as pastas de saída conforme o tipo de arquivo
+def get_output_folder_for_file_type(folder_name):
+    if 'industrialized' in folder_name:
+        return os.environ['INDUSTRIALIZED_PARQUET']
+    elif 'manipulated' in folder_name:
+        return os.environ['MANIPULATED_PARQUET']
+    elif 'raw_sim_files' in folder_name:
+        return os.environ['SIM_PARQUET']
+    else:
+        return os.environ['IBGE_PARQUET']
